cea13d2a1ece68d343b30e807ea2177dd0b763e9
Lam Phuc Duy <lamphucduy@yahoo.com>
2013-10-08 16:58:44 +0900

    Androidで画像をアップロード対応（途中）

---
+++ src/com/hackathon/fshow/ItemResgisterActivity.java
@@ -0,0 +1,12 @@
+package com.hackathon.fshow;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ItemResgisterActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
--- src/com/hackathon/fshow/test/TestBase64Bitmap.java
+++ src/com/hackathon/fshow/test/TestBase64Bitmap.java
@@ -29,7 +29,7 @@ public class TestBase64Bitmap extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.test_image_base64);
 		textView = (TextView) findViewById(R.id.textView1);
-		image1 = (ImageView) findViewById(R.id.imageView1);
+		image1 = (ImageView) findViewById(R.id.imageViewUpload);
 		image2 = (ImageView) findViewById(R.id.imageView2);
 
 		Runnable runnable = new Runnable() {
